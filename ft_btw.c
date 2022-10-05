e30dd23d5f9e3b7cc32fe3d20dd18fe737194643
rmaes <ikbenruudmaes@gmail.com>
2022-10-05 18:11:52 +0200

    update ft_btw to work even if d and i are flipped around

--- ft_btw.c
+++ ft_btw.c
@@ -6,7 +6,7 @@
 /*   By: rmaes <rmaes@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/04 18:03:51 by rmaes         #+#    #+#                 */
-/*   Updated: 2022/10/04 18:04:01 by rmaes         ########   odam.nl         */
+/*   Updated: 2022/10/05 18:10:59 by rmaes         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,10 @@
 // make sure n is between d and i
 int	ft_btw(int n, int d, int i)
 {
-	return (ft_min(ft_max(n, i), d));
+	if (i < d)
+		return (ft_max(ft_min(n, i), d));
+	else if (i == d)
+		return (d);
+	else if (d < i)
+		return (ft_max(ft_min(n, d), i));
 }
